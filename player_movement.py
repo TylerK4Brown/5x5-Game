67bc0ea0581d3fb9ffaab6f942420267e57eaa46
TylerK4Brown <tylerk4brown@gmail.com>
2025-12-27 16:17:07 -0500

    Adding the main game logic files

---
+++ player_movement.py
@@ -0,0 +1,48 @@
+# MAXIMUM DIMENSIONS ARE AS FOLLOWS:
+# up (w) = 100
+# down (s) = 300
+# left (a) = 200
+# right (d) = 400
+
+import pygame
+
+def redraw(player_pos, screen):
+    pygame.draw.rect(screen, "black", pygame.Rect(player_pos[0] - 12.5, player_pos[1] - 12.5, 30, 30), width=0)
+    pygame.draw.circle(screen, "white", [player_pos[0], player_pos[1]], 5)
+
+def move_player(player_pos, screen):
+    # NEGATIVE VALUES MOVE LEFT/UP, POSITIVE VALUES MOVE RIGHT/DOWN
+    keys = pygame.key.get_just_pressed()
+    if keys[pygame.K_w]:
+        if player_pos[1] <= 100:
+            pass
+        else:
+            redraw(player_pos, screen)
+            player_pos[1] -= 50
+            print(player_pos)
+    
+    if keys[pygame.K_s]:
+        if player_pos[1] >= 300:
+            pass
+        else:
+            redraw(player_pos, screen)
+            player_pos[1] += 50
+            print(player_pos)
+        
+    if keys[pygame.K_a]:
+        if player_pos[0] <= 200:
+            pass
+        else:
+            redraw(player_pos, screen)
+            player_pos[0] -= 50
+            print(player_pos)
+            
+    if keys[pygame.K_d]:
+        if player_pos[0] >= 400:
+            pass
+        else:
+            redraw(player_pos, screen)
+            player_pos[0] += 50
+            print(player_pos)
+
+    return player_pos
