85bb3a78eeab7d3984874e106c0f52a3cebaf093
TylerK4Brown <tylerk4brown@gmail.com>
2026-03-13 13:22:01 -0400

    REFACTOR: Circle printing logic is now a part of a loop rather than it being repeated for each circle that was missed

--- initialize_game/game_initializer.py
+++ initialize_game/game_initializer.py
@@ -14,35 +14,19 @@ def pygame_initialize():
 
 def draw_5x5_grid(screen):
     middle = [screen.get_width() / 2, screen.get_height() / 2]
-    # essentially, to keep this rectangle centered, we need to subtract half the width and height from the middle point
-    # the middle point is (275, 250), so we subtract 137.5 from x and 125 from y
+    # essentially, to keep this rectangle centered, we need to subtract half the width and height
+    # the width/height for our rect is (275, 250), so we subtract 137.5 from x and 125 from y
     rect_pos = pygame.Rect(middle[0] - 137.5, middle[1] - 125, 275, 250)
     pygame.draw.rect(screen, "white", rect_pos, width=5)
 
-    # this logic draws most of the grid spaces but misses like 8 of them I think
-    # the solution below is brute forced for the time being
+    # loops across the circle positions list
+    # prints each circle in a vertical fashion by displaying a circle at position -100 -> 100 for each pos_horizontal
+    # TODO: O(n^2) solution we must kill him
     circle_positions = [-100, -50, 0, 50, 100]
-    for pos in circle_positions:
-        pygame.draw.circle(screen, "white", [middle[0] + pos, middle[1]], 5)
-        pygame.draw.circle(screen, "white", [middle[0], middle[1] + pos], 5)
-        pygame.draw.circle(screen, "white", [middle[0] + pos, middle[1] + pos], 5)
-        pygame.draw.circle(screen, "white", [middle[0] + pos, middle[1] - pos], 5)
-        
-    # NEGATIVE VALUES MOVE LEFT/UP, POSITIVE VALUES MOVE RIGHT/DOWN
-    # I'm so sure there's a better way to do this but idgaf it is what it is    
-    # TODO: fix this bullshit
-    # top of screen
-    pygame.draw.circle(screen, "white", [middle[0] - 50, middle[1] - 100], 5)
-    pygame.draw.circle(screen, "white", [middle[0] + 50, middle[1] - 100], 5)
-    # bottom of screen
-    pygame.draw.circle(screen, "white", [middle[0] + 50, middle[1] + 100], 5)
-    pygame.draw.circle(screen, "white", [middle[0] - 50, middle[1] + 100], 5)
-    # right of screen
-    pygame.draw.circle(screen, "white", [middle[0] + 100, middle[1] - 50], 5)
-    pygame.draw.circle(screen, "white", [middle[0] + 100, middle[1] + 50], 5)
-    # left of screen
-    pygame.draw.circle(screen, "white", [middle[0] - 100, middle[1] - 50], 5)
-    pygame.draw.circle(screen, "white", [middle[0] - 100, middle[1] + 50], 5)
-    # update the screen
+    for pos_horizontal in circle_positions:
+        for pos_vertical in circle_positions:
+            pygame.draw.circle(screen, "white", [middle[0] + pos_horizontal, middle[1] + pos_vertical], 5)
+    
+    # update the display
     pygame.display.flip()
 
