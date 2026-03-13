9ac0b70635e3ef2871e8bcbe4f3bcf958ffa74ae
TylerK4Brown <tylerk4brown@gmail.com>
2026-03-13 10:52:19 -0400

    New "warning" flag in queue to indicate what type of obstacle should be spawned in the queue

--- obstacle_info/Obstacle_oop.py
+++ obstacle_info/Obstacle_oop.py
@@ -38,14 +38,22 @@ class Obstacles:
             # dequeue the top most element, get the grid space information from the 1st index in the tuple
             # obstacle_definition looks like [x, y]
             obstacle_definition = self.obstacle_queue.popleft()
-            grid_space = obstacle_definition[1]
+            grid_space = obstacle_definition[2]
+            # OLD IMPLEMENTATION:
             # add the obstacle to the screen, create a hitbox rect at that grid space
-            screen.blit(self.obstacle_img_surface, (grid_space[0] - 15, grid_space[1] - 15))
-            obstacle_hitbox = pygame.Rect(grid_space[0] - 15, grid_space[1] - 15, 30, 30)
-            # draws the hitbox to the screen (not necessary later on - just for visuals rn)
-            pygame.gfxdraw.rectangle(screen, obstacle_hitbox, (255, 0, 0, 255))
-            self.obstacle_list.append(obstacle_hitbox)
-    
+            # screen.blit(self.obstacle_img_surface, (grid_space[0] - 15, grid_space[1] - 15))
+            
+            # NEW IMPLEMENTATION:
+            # queue now holds a "warning" or "obstacle" indicator
+            # if the current value is "warning", draw a yellow square - draw a red square otherwise
+            obstacle_warning_space = pygame.Rect(grid_space[0] - 15, grid_space[1] - 15, 30, 30)
+            if obstacle_definition[1] != "warning":
+                screen.blit(self.obstacle_img_surface, (grid_space[0] - 15, grid_space[1] - 15))
+                pygame.gfxdraw.rectangle(screen, obstacle_warning_space, (255, 0, 0, 255))
+                self.obstacle_list.append(obstacle_warning_space)
+            else:
+                pygame.gfxdraw.box(screen, obstacle_warning_space, (255, 255, 0, 128))
+            
     # checks to see if the player's current position interacts with the current list of obstacles
     # if there are no obstacles in the obstacle list at the moment, return True
     # if any hitboxes overlap, return False to kill the program's running loop
--- obstacle_info/obstacle_queues.py
+++ obstacle_info/obstacle_queues.py
@@ -8,9 +8,9 @@ from collections import deque
 
 def create_queue(song_name):
     # song_name is not used yet since we only have obstacles appearing for one song
-    py_game_song_1_queue = [(100, [200, 200]), (737, [200, 100]), (1164, [200, 300]), (1680, [400, 100]), 
-    (1930, [400, 200]), (2200, [400, 300]), (2328, [300, 100]), (2856, [250, 150]), 
-    (3293, [350, 250]), (3780, [350, 150]), (3940, [250, 250]), (4170, [300, 300])]
+    py_game_song_1_queue = [(100, "warning", [200, 200]), (737, "obstalce", [200, 100]), (1164, "warning", [200, 300]), (1680, "warning", [400, 100]), 
+    (1930, "warning", [400, 200]), (2200, "obstacle", [400, 300]), (2328, "warning", [300, 100]), (2856, "warning", [250, 150]), 
+    (3293, "warning", [350, 250]), (3780, "warning", [350, 150]), (3940, "warning", [250, 250]), (4170, "warning", [300, 300])]
     
     queue = deque(py_game_song_1_queue)
     return queue
